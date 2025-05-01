8ab99a289cf6b2906a008e33f5065f6e41a78262
eyob231 <eyobs573@gmail.com>
2025-05-01 10:42:44 +0300

    Updates

---
+++ static/js/198.ed834273.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksupabase=self.webpackChunksupabase||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.ed834273.chunk.js.map
